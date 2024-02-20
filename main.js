ddcf4137b7f665b36eac144cfd72d0895b4a67ee
yangshoong <crinaeae83@gmail.com>
2024-02-21 01:43:48 +0900

    redirect

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let news = [];
 
 const getLatestNews = async () => {
     let url = new URL(
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+        `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`
     );
 
     const response = await fetch(url);
