bc743d1993328852ea7ee79576fa0e00d4b22a24
yangshoong <crinaeae83@gmail.com>
2024-02-22 01:16:30 +0900

    1st submit

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
 const API_KEY = '099d71dcaa30404d9ad3afb751957330'
-let news = [];
+let newsList = [];
 
 const getLatestNews = async () => {
     let url = new URL(
-        `https://newstimes-ost.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=099d71dcaa30404d9ad3afb751957330`
     );
 
     const response = await fetch(url);
     const data = await response.json();
-    news = data.articles;
+    newsList = data.articles;
+    render();
 
     console.log("rrrr", response);
-    console.log("dddd", news);
+    console.log("dddd", newsList);
 }
 
-getLatestNews();
-for (let i = 0; i < 20; i++) {
-    console.log("after", i)
+const render = () => {
+    const newsHTML = newsList.map(
+        (news) => ` <div class="row news">
+    <div class="col-lg-4">
+        <img class="news-img-size" src=${news.urlToImage ? news.urlToImage : 'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg'} alt="Image Not Available">
+    </div>
+    <div class="col-lg-8">
+        <h2>${news.title}</h2>
+        <p>${news.description ? news.description : '내용없음'}</p>
+        <div>${news.source.name ? news.source.name : 'no source'} * ${moment(news.publishedAt).fromNow()}</div>
+    </div>
+</div>`).join('');
+    document.getElementById("news-board").innerHTML = newsHTML;
 }
+
+
+getLatestNews();
