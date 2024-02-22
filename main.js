943cc8b66f3ad473d67b0d5a301e47d329fbd184
Yangshoong <crinaeae83@gmail.com>
2024-02-22 18:38:21 +0900

    search toggle

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const API_KEY = '099d71dcaa30404d9ad3afb751957330'
 let newsList = [];
 
-
+let searchIcon = document.getElementById("search-icon");
+let searchInput = document.getElementById("search-input");
+let searchButton = document.getElementById("search-button");
 
 
 const getLatestNews = async () => {
@@ -10,13 +12,13 @@ const getLatestNews = async () => {
     );
 
 
-const response = await fetch(url);
-const data = await response.json();
-newsList = data.articles;
-render();
+    const response = await fetch(url);
+    const data = await response.json();
+    newsList = data.articles;
+    render();
 
-console.log("rrrr", response);
-console.log("dddd", newsList);
+    console.log("rrrr", response);
+    console.log("dddd", newsList);
 }
 
 const render = () => {
@@ -27,7 +29,7 @@ const render = () => {
     </div>
     <div class="col-lg-8">
         <h2>${news.title}</h2>
-        <p>${news.description ? news.description.substring(0,100) : '내용없음'}...</p>
+        <p>${news.description ? news.description.substring(0, 100) : '내용없음'}...</p>
         <div>${news.source.name ? news.source.name : 'no source'} * ${moment(news.publishedAt).fromNow()}</div>
     </div>
 </div>`).join('');
@@ -36,3 +38,25 @@ const render = () => {
 
 
 getLatestNews();
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let searchIcon = document.getElementById("search-icon");
+    let searchInput = document.getElementById("search-input");
+    let searchButton = document.getElementById("search-button");
+  
+    searchIcon.addEventListener("click", function (event) {
+      // 'collapse' 클래스의 존재 여부를 토글하여 요소의 표시 상태를 변경합니다.
+      searchInput.classList.toggle("collapse");
+      searchButton.classList.toggle("collapse");
+    });
+  });
+  
+
+searchButton.addEventListener("click", function (event) {
+    if (searchInput.value.trim() === "") {
+        alert("검색어를 입력해주세요");
+        return;
+    }
+
+});
