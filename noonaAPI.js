0d91247452f9310392b75b80ea7150499176209f
Yangshoong <crinaeae83@gmail.com>
2024-02-23 18:36:28 +0900

    files remake

--- noonaAPI.js
+++ noonaAPI.js
@@ -1,32 +1,7 @@
 
 //New York Times API
-const getLatestNews = async () => {
-    let url = new URL(
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
-    );
+`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
 
-
-const response = await fetch(url);
-const data = await response.json();
-newsList = data.articles;
-render();
-
-console.log("rrrr", response);
-console.log("dddd", newsList);
-}
 
 //Netlify_noonaAPI
-const getLatestNews = async () => {
-    let url = new URL(
-        `https://newstimes-ost.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`
-    );
-
-
-const response = await fetch(url);
-const data = await response.json();
-newsList = data.articles;
-render();
-
-console.log("rrrr", response);
-console.log("dddd", newsList);
-}
+`https://newstimes-ost.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`
