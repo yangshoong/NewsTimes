48b29c1eb7c71e72b6b1c04aad5f8ac055065d96
yangshoong <crinaeae83@gmail.com>
2024-02-24 23:30:19 +0900

    pagination

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const API_KEY = '099d71dcaa30404d9ad3afb751957330';
 let newsList = [];
+let totalResults = 0
+let page = 1
+const pageSize = 10
+const groupSize = 5
 
-document.addEventListener('DOMContentLoaded', function () {
+
+document.addEventListener('DOMContentLoaded', async function () {
     const searchIcon = document.getElementById("search-icon");
     const searchInput = document.getElementById("search-input");
     const searchButton = document.getElementById("search-button");
@@ -22,26 +27,33 @@ document.addEventListener('DOMContentLoaded', function () {
             getNewsByKeyword();
         }
     });
-
-    getNews('top-headlines?country=us');
+    await getNews();
 });
 
-async function getNews(endpoint) {
+async function getNews(endpoint = 'top-headlines?country=us') {
     try {
-        const url = `https://newstimes-ost.netlify.app/${endpoint}&apiKey=${API_KEY}`;
+        const url = new URL(`https://newsapi.org/v2/${endpoint}&apiKey=${API_KEY}`);
+        url.searchParams.set("page", page)
+        url.searchParams.set("pageSize", pageSize);
         const response = await fetch(url);
+
+
         const data = await response.json();
         if (response.status === 200) {
             if (data.articles.length === 0) {
                 throw new Error("No matches for your search")
             }
             newsList = data.articles;
+            totalResults = data.totalResults;
             render();
+            console.log('render');
+            paginationRender();
         } else {
             throw new Error(data.message)
         }
     } catch (error) {
-        errorRender(error.message)
+        console.error(error);
+        errorRender(error.message);
     }
 }
 
@@ -85,3 +97,30 @@ const errorRender = (errorMessage) => {
     document.getElementById("news-board").innerHTML = errorHTML
 }
 
+const paginationRender = () => {
+    const pageGroup = Math.ceil(page / groupSize);
+    const totalPages = Math.ceil(totalResults / pageSize)
+    let lastPage = pageGroup * groupSize;
+
+    if (lastPage > totalPages) {
+        lastPage = totalPages;
+    }
+    const firstPage = lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
+
+    let paginationHTML = `<li class="page-item"><a class="page-link" onclick="moveToPage(${page - 1})">Previous</a></li>`;
+
+    for (let i = firstPage; i <= lastPage; i++) {
+        paginationHTML += `<li class="page-item ${i === page ? "active" : ""}" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`;
+    };
+
+    paginationHTML += `<li class="page-item"><a class="page-link" onclick="moveToPage(${page + 1})">Next</a></li>`;
+
+    document.querySelector(".pagination").innerHTML = paginationHTML
+}
+
+
+const moveToPage = (pageNum) => {
+    console.log("moveToPage", pageNum);
+    page = pageNum;
+    getNews();
+};
